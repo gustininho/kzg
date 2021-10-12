71c617d3bb781d9b7771717c42036e11ea619cfe
Yar3k <djelement567@gmail.com>
2021-10-12 21:54:40 +0300

    add fr-zero test

--- kzg10-rust/kzg-bench/src/bls12_381.rs
+++ kzg10-rust/kzg-bench/src/bls12_381.rs
@@ -1,14 +1,21 @@
+use std::{mem, vec};
 use mcl_rust::old::*;
+use mcl_rust::utilities::*;
 use mcl_rust::CurveType;
 use mcl_rust::data_types::fr::Fr;
-use mcl_rust::mlc_methods::*;
-use mcl_rust::utilities::*;
+use mcl_rust::mlc_methods::init;
 
 #[test]
-fn log_2_byte_works() {
+fn alog_2_byte_works() {
     assert!(0 == log_2(0x01));
     assert!(7 == log_2(0x80));
     assert!(7 == log_2(0xff));
     assert!(4 == log_2(0x10));
 }
 
+#[test]
+fn fr_is_zero_works() {
+    assert!(init(CurveType::BLS12_381));
+    let x = Fr::from_int(0);
+    assert!(true);
+}

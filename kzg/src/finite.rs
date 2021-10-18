6e41ebf8f4b433c8c6c3717be0455cd761e37d6d
Saulius Grigaitis <saulius@sifrai.com>
2021-10-18 23:23:25 +0300

    Added example using New Type idiom

--- kzg/src/finite.rs
+++ kzg/src/finite.rs
@@ -3,7 +3,7 @@ use super::Fr;
 pub fn add_fr(first: Fr, second: Fr) -> Fr {
     let mut sum = Fr::default();
     unsafe {
-        blst::blst_fr_add(&mut sum, &first, &second);
+        blst::blst_fr_add(&mut sum.0, &first.0, &second.0);
     }
     sum
 }
